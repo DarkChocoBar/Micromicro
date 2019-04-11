977f4983c3e9fcea915b7102484da8239149c0ba
DarkChocoBar <32530701+DarkChocoBar@users.noreply.github.com>
2019-04-10 22:13:28 -0700

    updated my own method to do LRU cache

--- leetcode/src/microsoft/LRUCache.java
+++ leetcode/src/microsoft/LRUCache.java
@@ -2,6 +2,134 @@ package microsoft;
 
 import java.util.HashMap;
 
+class Solution2 {
+	class LRUCache {
+
+	    class DoubleLinkedList {
+	        int val;
+	        int key;
+	        DoubleLinkedList prev;
+	        DoubleLinkedList next;
+	    }
+	    
+	    HashMap<Integer, DoubleLinkedList> cache;
+	    
+	    int count = 0;
+	    int curCapacity = 0;
+	    
+	    DoubleLinkedList head;
+	    DoubleLinkedList tail;
+	    
+	    public LRUCache(int capacity) {
+	        this.count = 0;
+	        this.curCapacity = capacity;
+	        
+	        this.cache = new HashMap<>();
+	        
+	        this.head = new DoubleLinkedList();
+	        this.tail = new DoubleLinkedList();
+	        this.head.prev = null;
+	        this.head.next = this.tail;
+	        this.tail.prev = this.head;
+	        this.tail.next = null;
+	        
+	    }
+	    
+	    public int get(int key) {
+	        if (cache.containsKey(key)) {
+	            // Todo: remove node first
+	            int ans = cache.get(key).val;
+	            
+	            DoubleLinkedList node = cache.get(key);
+	            removeNode(node);
+	            node.prev = this.head;
+	            node.next = this.head.next;
+	            this.head.next.prev = node;
+	            this.head.next = node;
+
+	            return ans;
+	        } else {
+	            return -1;
+	        }
+	    }
+	    
+	    public void put(int key, int value) {
+	        if (this.count < this.curCapacity) {
+	            if (cache.containsKey(key)) {
+	                // Todo: remove node first
+	                DoubleLinkedList node = cache.get(key);
+	                removeNode(node);
+	                
+	                // update node value
+	                node.val = value;
+	                
+	                // put node in the head
+	                node.prev = this.head;
+	                node.next = this.head.next;
+	                this.head.next.prev = node;
+	                this.head.next = node;
+	                
+	                // update cache
+	                cache.put(key, node);
+	            } else {
+	                DoubleLinkedList node = new DoubleLinkedList();
+	                node.key = key;
+	                node.val = value;
+	                node.prev = this.head;
+	                node.next = this.head.next;
+	                this.head.next.prev = node;
+	                this.head.next = node;
+	                this.count++;
+	                
+	                cache.put(key, node);
+	            }
+	        } else {
+	            if (cache.containsKey(key)) {
+	                // Todo: remove node first
+	                DoubleLinkedList node = cache.get(key);
+	                removeNode(node);
+	                node.val = value;
+	                node.prev = this.head;
+	                node.next = this.head.next;
+	                this.head.next.prev = node;
+	                this.head.next = node;
+	                
+	                cache.put(key, node);
+	            } else {
+	                DoubleLinkedList node = new DoubleLinkedList();
+	                node.key = key;
+	                node.val = value;
+	                node.prev = this.head;
+	                node.next = this.head.next;
+	                this.head.next.prev = node;
+	                this.head.next = node;
+	                
+	                // remove tail
+	                cache.remove(this.tail.prev.key);
+	                
+	                removeNode(this.tail.prev);
+	                
+	                cache.put(key, node);
+	            }
+	        }
+	    }
+	    
+	    public void removeNode(DoubleLinkedList node) {
+	        DoubleLinkedList prev = node.prev;
+	        DoubleLinkedList next = node.next;
+	        prev.next = next;
+	        next.prev = prev;
+	    }
+	}
+
+	/**
+	 * Your LRUCache object will be instantiated and called as such:
+	 * LRUCache obj = new LRUCache(capacity);
+	 * int param_1 = obj.get(key);
+	 * obj.put(key,value);
+	 */
+}
+
 public class LRUCache {
 	    class DoubleLinkedNode {
 	        int val;
@@ -10,9 +138,6 @@ public class LRUCache {
 	        DoubleLinkedNode prev;
 	        
 	    }
-	    
-	    private int key;
-	    private int val;
 	    
 	    // for calculating the capacity and current content count in the hashmap
 	    private int capacity;
